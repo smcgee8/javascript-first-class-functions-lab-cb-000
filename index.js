32056a14897fd879e3fc25b62544246ad4e1a2ba
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-21 13:44:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function countdown(callback) {
   window.setTimeout(callback, 2000);
 }
+
+function createMultiplier(multiplierValue) {
+  return function(input) {return input * multiplierValue;}
+}
+
+var doubler = createMultiplier(2);
+var tripler = createMultiplier(3);
