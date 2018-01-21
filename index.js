a8444f748afaa3316d1aad01ade8a07a77a349e3
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-21 13:52:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var doubler = createMultiplier(2);
 var tripler = createMultiplier(3);
 
 function multiplier(multiplierValue, input) {
-
+  return input * multiplierValue;
 }
 
 var doublerWithBind = multiplier.bind(null, 2);
